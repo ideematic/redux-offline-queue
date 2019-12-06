20d9d372015d36d57e7680ef3ab74994a50c7cd9
Fabrice DIDIERJEAN <f.didierjean@ideematic.com>
2019-12-06 15:26:17 +0100

    custom queue actions

--- src/reducer.js
+++ src/reducer.js
@@ -9,25 +9,35 @@ import { QUEUE_ACTION, ONLINE, OFFLINE, RESET_QUEUE } from './actions'
  * @param {Object} state Offline queue Redux store state.
  * @param {Object} action Action that was dispatched to the store.
  */
-export default function reducer(state = INITIAL_STATE, action = {}) {
-  switch (action.type) {
-    case REHYDRATE: { // Handle rehydrating with custom shallow merge.
 
-      if (action.payload && action.payload.offline) {
-        return { ...state, ...action.payload.offline };
+ const internalQueue = (queue, payload) => {
+   return queue.concat(payload)
+ }
+
+export const customActionQueueReducer = (funcActionQueue) => {
+  function _reducer(state = INITIAL_STATE, action = {}) {
+    switch (action.type) {
+      case REHYDRATE: { // Handle rehydrating with custom shallow merge.
+
+        if (action.payload && action.payload.offline) {
+          return { ...state, ...action.payload.offline };
+        }
+
+        return state
       }
-
-      return state
+      case QUEUE_ACTION:
+        return { ...state, queue: funcActionQueue(state.queue, action.payload) }
+      case ONLINE:
+        return { ...state, isConnected: true }
+      case OFFLINE:
+        return { ...state, isConnected: false }
+      case RESET_QUEUE:
+        return { ...state, queue: [] }
+      default:
+        return state
     }
-    case QUEUE_ACTION:
-      return { ...state, queue: state.queue.concat(action.payload) }
-    case ONLINE:
-      return { ...state, isConnected: true }
-    case OFFLINE:
-      return { ...state, isConnected: false }
-    case RESET_QUEUE:
-      return { ...state, queue: [] }
-    default:
-      return state
   }
+  return _reducer
 }
+
+export default customActionQueueReducer(internalQueue)
