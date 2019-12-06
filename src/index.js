bec8c8bb158b4e2e9f263f4bb6ebe6d9c2a32a39
Fabrice DIDIERJEAN <f.didierjean@ideematic.com>
2019-12-06 15:29:31 +0100

    export customQueue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as actions from './actions'
 import offlineMiddleware from './offlineMiddleware'
 import { createOfflineActions, markActionsOffline } from './offlineActions'
-import reducer from './reducer'
+import reducer, { customActionQueueReducer } from './reducer'
 import suspendSaga from './suspendSaga'
 import consumeActionMiddleware from './consumeActionMiddleware'
 import offlinePersistenceTransform from './offlinePersistenceTransform'
@@ -16,4 +16,5 @@ module.exports = {
   suspendSaga,
   consumeActionMiddleware,
   offlinePersistenceTransform,
+  customActionQueueReducer
 }
